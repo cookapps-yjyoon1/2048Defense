68c7c99df232114739a907ac8f6daf5b55fd7cee
yjyoon1 <yjyoon1@cookapps.com>
2024-03-22 16:14:17 +0900

    난이도 추가

--- Assets/Project/Scripts/MainScenario.cs
+++ Assets/Project/Scripts/MainScenario.cs
@@ -1,39 +1,65 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainScenario : MonoBehaviour
 {
-    private int matrixIndex = 0;
+    [SerializeField] private Text _txtStage;
+    [SerializeField] private GameObject _goIsClear;
+    [SerializeField] private GameObject _goLeft;
+    [SerializeField] private GameObject _goRight;
+    
+    private int _curStageIndex = 0;
 
     public void OnEnable()
     {
         SoundManager.Instance.Play(Enum_Sound.Bgm, "Lobby");
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        var isClear = PlayerDataManager.ETCData.IsClearStage[_curStageIndex];
+        
+        _txtStage.text = $"{_curStageIndex+1}";
+        _goIsClear.SetActive(isClear);
+        
+        _goLeft.SetActive(_curStageIndex > 0);
+        _goRight.SetActive(_curStageIndex < PlayerDataManager.ETCData.IsClearStage.Count-1);
     }
 
     public void OnClickGameStart()
     {
+        PlayerDataManager.ETCData.CurStage = _curStageIndex;
         SceneManager.LoadScene("02Game");
     }
-
-    public void OnClickGameExit()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.ExitPlaymode();
-#else
-		Application.Quit();
-#endif
-    }
-
+    
     public void OnClickLeft()
     {
+        if (_curStageIndex <= 0)
+        {
+            return;
+        }
+        
+        _curStageIndex--;
+
+        Refresh();
+        
         
         SoundManager.Instance.Play(Enum_Sound.Effect, "MoveStage");
-
     }
 
     public void OnClickRight()
     {
+        if (_curStageIndex >= PlayerDataManager.ETCData.IsClearStage.Count-1)
+        {
+            return;
+        }
 
+        _curStageIndex++;
+
+        Refresh();
+        
         SoundManager.Instance.Play(Enum_Sound.Effect, "MoveStage");
     }
 }
--- Assets/Project/Scripts/PlayerData/ETCData.cs
+++ Assets/Project/Scripts/PlayerData/ETCData.cs
@@ -1,14 +1,27 @@
+using System.Collections.Generic;
+
 public class ETCData : GameData
 {
     protected override eDataType DataType => eDataType.ETC;
 
+    public List<bool> IsClearStage = new List<bool>();
+    public int CurStage = 0;
+    
     public override void Initialize()
     {
-        
+        for (int i = IsClearStage.Count; i < 9; i++)
+        {
+            IsClearStage.Add(false);
+        }
     }
 
     public override void LateInitialize()
     {
         
+    }
+    
+    public void SetClearStage(int index)
+    {
+        IsClearStage[index] = true;
     }
 }
